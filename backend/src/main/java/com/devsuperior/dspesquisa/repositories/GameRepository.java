68eeb406b1b66395bc534f785dc175bda7651ddf
Ivander <ivandersalvadorruiz@gmail.com>
2020-09-14 17:46:29 -0300

    Implemented GameDTO and GameService

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/GameRepository.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.devsuperior.dspesquisa.entities.Genre;
+import com.devsuperior.dspesquisa.entities.Game;
 
-public interface GameRepository extends JpaRepository<Genre, Long> {
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
 }
